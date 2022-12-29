f3f5bf6bf715e2ebd5c55ccc35649f5e59ea2b2f
devCamposs <thomas.campos7@gmail.com>
2022-12-29 11:55:16 -0300

    componetes

--- src/assets/components/App.js
+++ src/assets/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import SideBar from './SideBar'
 import ContentWrapper from './ContentWrapper'
+// import {BrowserRouterreact, Link, Route} from '-router-dom'
 
 function App(){
   return (
@@ -15,28 +16,4 @@ function App(){
 
 export default App
 
-// import logo from './logo.svg';
-// import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
--- src/assets/components/ContentRowMovies.js
+++ src/assets/components/ContentRowMovies.js
@@ -1,6 +1,7 @@
 import React from "react";
+import propTypes from 'prop-types'
 
-function ContentRowMovies() {
+function ContentRowMovies(props) {
   return (
     <React.Fragment>
 
@@ -9,59 +10,46 @@ function ContentRowMovies() {
 
         {/*<!-- Movies in Data Base -->*/}
         <div className="col-md-4 mb-4">
-          <div className="card border-left-primary shadow h-100 py-2">
+          <div className={"card" + props.dado.borderColor + "shadow h-100 py-2"}>
             <div className="card-body">
               <div className="row no-gutters align-items-center">
                 <div className="col mr-2">
-                  <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">Movies in data base</div>
-                  <div className="h5 mb-0 font-weight-bold text-gray-800">21</div>
+                  <div className={"text-xs font-weight-bold text-" + props.dado.textColor + "text-uppercase mb-1"}>{props.dado.title}</div>
+                  <div className="h5 mb-0 font-weight-bold text-gray-800">{props.dado.qtd}</div>
                 </div>
                 <div className="col-auto">
-                  <i className="fas fa-film fa-2x text-gray-300"></i>
+                  <i className={"fas" + props.dado.icon + "fa-2x text-gray-300"}></i>
                 </div>
               </div>
             </div>
           </div>
         </div>
 
-        {/*<!-- Total awards -->*/}
-        <div className="col-md-4 mb-4">
-          <div className="card border-left-success shadow h-100 py-2">
-            <div className="card-body">
-              <div className="row no-gutters align-items-center">
-                <div className="col mr-2">
-                  <div className="text-xs font-weight-bold text-success text-uppercase mb-1"> Total awards</div>
-                  <div className="h5 mb-0 font-weight-bold text-gray-800">79</div>
-                </div>
-                <div className="col-auto">
-                  <i className="fas fa-award fa-2x text-gray-300"></i>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
 
-        {/*<!-- Actors quantity --> */}
-        <div className="col-md-4 mb-4">
-          <div className="card border-left-warning shadow h-100 py-2">
-            <div className="card-body">
-              <div className="row no-gutters align-items-center">
-                <div className="col mr-2">
-                  <div className="text-xs font-weight-bold text-warning text-uppercase mb-1">Actors quantity
-                  </div>
-                  <div className="h5 mb-0 font-weight-bold text-gray-800">49</div>
-                </div>
-                <div className="col-auto">
-                  <i className="fas fa-user fa-2x text-gray-300"></i>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
       {/* <!-- End movies in Data Base --> */}
     </React.Fragment>
   )
+}
+ContentRowMovies.propTypes = {
+  dado: propTypes.shape({
+    title: propTypes.string.isRequired,
+    icon: propTypes.string.isRequired,
+    qtd: propTypes.number.isRequired,
+    textColor: propTypes.string.isRequired,
+    borderColor: propTypes.oneOfType([
+      propTypes.string,
+      propTypes.oneOf(['border-left-primary', 'border-left-sucess', 'border-left-warning']),
+    ]).isRequired
+  }).isRequired
+}
+
+ContentRowMovies.defaultProps = {
+  title: 'Star',
+  icon: 'fa-film',
+  qtd: 9,
+  borderColor: 'border-left-sucess',
+  textColor: 'sucess'
 }
 
 export default ContentRowMovies
